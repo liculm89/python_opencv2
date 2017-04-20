b16f506ee8d809035788da42bfe6902ab010bb7c
Mauro Licul <liculm89@gmail.com>
2017-04-20 14:53:07 +0200

    first commit

---
+++ multiple_patern.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 14:38:35 2017
+
+@author: mauro
+"""
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img_rgb = cv2.imread('ring_cad2.jpg')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+template = cv2.imread('uzorak3.jpg',0)
+w, h = template.shape[::-1]
+
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+threshold = 0.95
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
+cv2.imwrite('res2.png',img_rgb)
+
+cv2.imshow('detected circles',img_rgb)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ test1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 09:00:12 2017
+
+@author: mauro
+"""
+
+import numpy as np
+import cv2
+import matplotlib
+print(matplotlib.matplotlib_fname())
+#matplotlib.use("Agg") 
+from matplotlib import pyplot as plt
+
+img1 = cv2.imread('ring_cad1.jpg', 0)
+img2 = cv2.imread('uzorak1.jpg', 0)
+
+orb = cv2.ORB_create()
+
+
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matches,
+                   flags = 0)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2, matches[:5], None, flags=2)
+
+plt.imshow(img3),plt.show()
